e84d75efed10bd8b38750404e13cd6075ae57bf4
Eric <em13914@users.noreply.github.com>
2018-01-29 12:45:40 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// These are all the different things you can do with audio files: https://www.w3schools.com/tags/ref_av_dom.asp
+
+function showText(e){
+	document.getElementById("text").style.display = "flex";
+}
+
+setTimeout(showText, 2000);
